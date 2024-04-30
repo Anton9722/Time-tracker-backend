e4de9ba9f1bb835e52b64e8742c0fbf7666c57a3
Anton Johansson <anton9797johansson@hotmail.com>
2024-04-30 14:20:47 +0200

    some endpoints

--- src/main/java/com/timetrackerbackend/timetrackerbackend/controllers/AccountController.java
+++ src/main/java/com/timetrackerbackend/timetrackerbackend/controllers/AccountController.java
@@ -1,5 +1,33 @@
 package com.timetrackerbackend.timetrackerbackend.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.timetrackerbackend.timetrackerbackend.models.Account;
+import com.timetrackerbackend.timetrackerbackend.services.AccountService;
+
+@RestController
 public class AccountController {
     
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //create new account
+    @PostMapping("/account/create")
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+
+    //get an account by id
+    @GetMapping("/account/get/{id}")
+    public Account getAccountById(@PathVariable String id) {
+        return accountService.getAccountById(id);
+    }
+
 }
--- src/main/java/com/timetrackerbackend/timetrackerbackend/services/TaskService.java
+++ src/main/java/com/timetrackerbackend/timetrackerbackend/services/TaskService.java
@@ -1,5 +1,38 @@
 package com.timetrackerbackend.timetrackerbackend.services;
 
+
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import com.timetrackerbackend.timetrackerbackend.models.Account;
+import com.timetrackerbackend.timetrackerbackend.models.Task;
+
+@Service
 public class TaskService {
     
+    private final MongoOperations mongoOperations;
+
+    public TaskService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    //create task
+    public Task createTask(Task task, String accountId) {
+        
+        mongoOperations.insert(task);
+
+        //find the account that created the task and then adds the task to its tasklist
+        Query query = new Query();
+        query.addCriteria(Criteria.where("id").is(accountId));
+        Account accountToGetTask = mongoOperations.findOne(query, Account.class);
+
+        //adds task to accounts taskList and the saves the changes
+        accountToGetTask.getTaskList().add(task);
+        mongoOperations.save(accountToGetTask);
+        
+        return task;
+    }
+
 }
