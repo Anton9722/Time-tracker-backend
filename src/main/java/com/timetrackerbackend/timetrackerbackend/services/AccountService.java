5ce0a914847c71ee361e88d6e4d6e31ec6f5df0a
Anton Johansson <anton9797johansson@hotmail.com>
2024-05-01 11:13:52 +0200

    endpoints

--- src/main/java/com/timetrackerbackend/timetrackerbackend/services/AccountService.java
+++ src/main/java/com/timetrackerbackend/timetrackerbackend/services/AccountService.java
@@ -1,11 +1,14 @@
 package com.timetrackerbackend.timetrackerbackend.services;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.timetrackerbackend.timetrackerbackend.models.Account;
+import com.timetrackerbackend.timetrackerbackend.models.Task;
 
 @Service
 public class AccountService {
@@ -26,6 +29,12 @@ public class AccountService {
         Query query = new Query();
         query.addCriteria(Criteria.where("id").is(id));
         return mongoOperations.findOne(query, Account.class);
+    }
+
+    //get all tasks in account
+    public List<Task> getTaskListById(String id) {
+        Account account = getAccountById(id);
+        return account.getTaskList();
     }
     
 }
